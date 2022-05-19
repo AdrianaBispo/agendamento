466effaece305396934ee6b23466a9a23d4bfab3
AdrianaBispo <adriana.bispo283@gmail.com>
2022-05-19 18:59:41 -0300

    selected efeito

--- lib/view/ui/drawer_list_tile.dart
+++ lib/view/ui/drawer_list_tile.dart
@@ -7,28 +7,49 @@ class DrawerListTile extends StatelessWidget {
   late String texto;
   late dynamic pagina;
   late String icon;
+  bool selected;
 
   DrawerListTile({
     required this.pagina,
     required this.texto,
     required this.icon,
+    required this.selected,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        Navigator.push(context,
-        MaterialPageRoute(builder: ((context) => pagina)));
-      },
-      horizontalTitleGap: 0.0,
-      leading: SvgPicture.asset('asset/icones/Icon/$icon.svg', width: 20, height: 20),
-      title: Text(
-        texto,
-        style: const TextStyle(
-          color: AppColor.natural,
-          fontSize: 14,
-          fontWeight: FontWeight.normal,
+    return Container(
+      margin: const EdgeInsets.all(5.0),
+      decoration: BoxDecoration(
+        color: selected ? AppColor.blue : AppColor.white,
+        border: Border.all(
+          width: 2,
+          color: selected ? AppColor.blue : AppColor.white,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ListTile(
+        selected: selected,
+        onTap: () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: ((context) => pagina)),
+          );
+        },
+        horizontalTitleGap: 0.0,
+        leading: SvgPicture.asset(
+          'asset/icones/Icon/$icon.svg',
+          color: selected ? AppColor.white : AppColor.natural_5,
+          width: 20,
+          height: 20,
+        ),
+        title: Text(
+          texto,
+          style: TextStyle(
+            color: selected ? AppColor.white : AppColor.natural_5,
+            fontSize: 14,
+            fontWeight: FontWeight.normal,
+          ),
         ),
       ),
     );
