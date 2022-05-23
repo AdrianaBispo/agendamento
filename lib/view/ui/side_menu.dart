3a62f4b39a3f727b9b75fc8dc376587355e5c4f9
AdrianaBispo <adriana.bispo283@gmail.com>
2022-05-23 19:41:22 -0300

    side menu class

---
+++ lib/view/ui/side_menu.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+//color
+import '../../const/colors.dart';
+//ui
+import 'package:agenda/view/ui/drawer_list_tile.dart';
+//view
+import '../home_page.dart';
+import '../clientes.dart';
+
+class SideMenu extends StatelessWidget {
+List<bool> selected;
+  SideMenu({required this.selected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColor.white,
+      child: Column(
+        children: <Widget>[
+          DrawerListTile(
+            pagina: HomePage(),
+            texto: 'Home',
+            icon: 'element',
+            selected: selected[0],
+          ),
+          DrawerListTile(
+            pagina: {},
+            texto: 'Agenda',
+            icon: 'calendar',
+            selected: selected[1],
+          ),
+          DrawerListTile(
+            pagina: ClientesPage(),
+            texto: 'Clientes',
+            icon: 'profile-2user',
+            selected: selected[2],
+          ),
+          //DrawerListTile(pagina: {}, texto: 'Profissionais'),
+        ],
+      ),
+    );
+  }
+}
