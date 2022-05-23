87dacf351ecdf4d3ebbf1c9294c110bcb7a7b987
AdrianaBispo <adriana.bispo283@gmail.com>
2022-05-23 19:19:28 -0300

    cliente view criado

---
+++ lib/view/clientes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+//color
+import '../const/colors.dart';
+
+class ClientesPage extends StatefulWidget {
+  const ClientesPage({Key? key}) : super(key: key);
+
+  @override
+  State<ClientesPage> createState() => _ClientesPageState();
+}
+
+class _ClientesPageState extends State<ClientesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
