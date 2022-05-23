5bd4ee2738ef8585665b4dfbd7ed7fea2f25019e
AdrianaBispo <adriana.bispo283@gmail.com>
2022-05-23 19:41:46 -0300

    side menu emplementado

--- lib/view/clientes.dart
+++ lib/view/clientes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 //color
 import '../const/colors.dart';
+//ui
+import 'ui/side_menu.dart';
 
 class ClientesPage extends StatefulWidget {
   const ClientesPage({Key? key}) : super(key: key);
@@ -13,7 +15,18 @@ class _ClientesPageState extends State<ClientesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1, //takes 1/6 of the screen
+                child: SideMenu(selected: [false, false, true, false]),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
