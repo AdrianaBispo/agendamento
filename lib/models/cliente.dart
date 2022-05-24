bb5606895efba6da16a414c1a307b065b8e64764
AdrianaBispo <adriana.bispo283@gmail.com>
2022-05-24 16:46:32 -0300

    cliente classe modelo

---
+++ lib/models/cliente.dart
@@ -0,0 +1,11 @@
+import 'historico.dart';
+class Cliente{
+  final String nome;
+  final String telefone;
+  final List<Historico> historico;
+  Cliente({
+    required this.nome,
+    required this.telefone,
+    this.historico = <Historico>[];
+  });
+}
