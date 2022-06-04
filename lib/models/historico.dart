3cc9227728600935e25b4af8fb905e5ed2a45dbe
AdrianaBispo <adriana.bispo283@gmail.com>
2022-06-04 19:42:48 -0300

    criado os metodos toJson e fromJson

--- lib/models/historico.dart
+++ lib/models/historico.dart
@@ -5,4 +5,19 @@ class Historico{
     required this.profissionalNome,
     required this.servico,
   });
+  Map<String, String> toJson() {
+    //convert para map
+    return {
+      'profissionalNome': profissionalNome,
+      'servico': servico,
+    };
+  }
+
+  factory Historico.fromJson(Map<String, dynamic> map) {
+    //toJson
+    return Historico(
+      profissionalNome: map['profissionalNome'], 
+      servico: map['servico'],
+      );
+  }
 }
