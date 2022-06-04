b333d19e9a9d45a134049fe62c5b1c40adfa5fea
AdrianaBispo <adriana.bispo283@gmail.com>
2022-06-04 19:47:14 -0300

    criado toJson e fromJson

--- lib/models/servicos.dart
+++ lib/models/servicos.dart
@@ -6,4 +6,19 @@ class Servicos{
     required this.nome,
     required this.duracao,
   });
-}
+Map<String, String> toJson() {
+    //convert para map
+    return {
+      'nome': nome,
+      'duracao': duracao,
+    };
+  }
+
+  factory Servicos.fromJson(Map<String, dynamic> map) {
+    //toJson
+    return Servicos(
+      nome: map['nome'], 
+      duracao: map['duracao'],
+      );
+  }
+  }
