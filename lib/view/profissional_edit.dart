2c9434fcf3d20646184b23060439982fa9830229
AdrianaBispo <adriana.bispo283@gmail.com>
2022-06-13 10:12:52 -0300

    container para o form criado

--- lib/view/profissional_edit.dart
+++ lib/view/profissional_edit.dart
@@ -5,6 +5,7 @@ import '../models/profissional.dart';
 //components
 import '../components/colors.dart';
 import '../components/side_menu.dart';
+import '../components/custom_appBar.dart';
 
 class ProfissionalEdit extends StatefulWidget {
   Profissional profissional;
@@ -18,19 +19,66 @@ class _ProfissionalEditState extends State<ProfissionalEdit> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      body: SafeArea(
         child: Row(
           children: [
             Expanded(
               flex: 1,
               child: Container(
-                  color: AppColor.white,
-                  child: SideMenu(
-                    selected: const [false, false, false, false],
-                  ) //sideMenu,
+                color: AppColor.white,
+                child: SideMenu(
+                  selected: const [false, false, false, false],
+                ), //sideMenu
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: SizedBox(
+                child: SingleChildScrollView(
+                  primary: false,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              color: AppColor.blue,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(30),
+                              ),
+                            ),
+                          ),
+                          CustoAppBar(
+                            texto: 'Editar',
+                          ),
+                        ],
+                      ), //Custo AppBar
+                      _formContainer(),
+                    ],
                   ),
+                ),
+              ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _formContainer() {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20, 40, 20, 20),
+      padding: const EdgeInsets.all(20),
+      width: double.infinity / 2,
+      height: 70,
+      decoration: const BoxDecoration(
+        color: AppColor.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(5),
         ),
       ),
     );
