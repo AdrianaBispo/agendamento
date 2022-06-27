54f4ee7f300d7e3269a576ad232381564d0ec494
AdrianaBispo <adriana.bispo283@gmail.com>
2022-06-26 21:35:58 -0300

    imports

--- lib/view/cliente_edit.dart
+++ lib/view/cliente_edit.dart
@@ -1,4 +1,16 @@
-
+import 'package:flutter/material.dart';
+//model
+import '../models/cliente.dart';
+import '../models/historico.dart';
+//repository
+import '../repository/validator.dart';
+import '../repository/cliente_controller.dart';//database
+//components
+import '../components/colors.dart';
+import '../components/side_menu.dart';
+import '../components/custom_appBar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_picker/flutter_picker.dart';
 
 class ClienteEdit extends StatefulWidget {
   Cliente cliente;
