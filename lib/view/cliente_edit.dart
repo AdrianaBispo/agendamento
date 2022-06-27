426f72a88161f7fe27dca3049378ce91a6608c31
AdrianaBispo <adriana.bispo283@gmail.com>
2022-06-26 21:35:36 -0300

    cliente Edit criado

---
+++ lib/view/cliente_edit.dart
@@ -0,0 +1,16 @@
+
+
+class ClienteEdit extends StatefulWidget {
+  Cliente cliente;
+  ClienteEdit({required this.cliente});
+
+  @override
+  State<ClienteEdit> createState() => _ClienteEditState();
+}
+
+class _ClienteEditState extends State<ClienteEdit> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
