1dc8a1f4a317cd3391912d308a4799d48b5d786d
AdrianaBispo <adriana.bispo283@gmail.com>
2022-06-29 17:13:24 -0300

    agendamento Page criado

---
+++ lib/view/agendamento_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+//repository
+import '../repository/validator.dart';
+import '../repository/cliente_controller.dart';
+//color
+import '../components/colors.dart';
+//ui
+import '../components/side_menu.dart';
+import '../components/custom_appBar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AgendamentoPage extends StatefulWidget {
+  AgendamentoPage({Key? key});
+
+  @override
+  State<AgendamentoPage> createState() =>_AgendamentoPage();
+}
+
+class _AgendamentoPage extends State<AgendamentoPage>{
+  @override
+  Widget build(BuildContext context ){
+    return Container();
+  }
+}
