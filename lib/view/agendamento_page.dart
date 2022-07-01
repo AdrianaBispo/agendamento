d65365aa1cfba9fd179923e9ce29dccc2cf8fb35
AdrianaBispo <adriana.bispo283@gmail.com>
2022-07-01 19:46:32 -0300

    custo app bar

--- lib/view/agendamento_page.dart
+++ lib/view/agendamento_page.dart
@@ -40,6 +40,7 @@ class _AgendamentoPage extends State<AgendamentoPage> {
               child: SizedBox(
                   child: ListView(
                 children: [
+                  CustoAppBar(texto: 'Agendamento'),
                   calendar(),
                 ],
               )),
