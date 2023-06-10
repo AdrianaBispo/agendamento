7aaad723397006a733c585a6d6b9b82e15c51baa
dev <adriana.bispo283@gmail.com>
2023-06-10 19:34:20 -0300

    feat: adcionado dependencia do ClienteHomeController

--- lib/src/appwidget.dart
+++ lib/src/appwidget.dart
@@ -1,8 +1,9 @@
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 //view
 import 'features/clients/home/home.dart';
 //utils
+import 'features/clients/home/store/clientehome_store.dart';
 import 'shared/utils/app_theme.dart';
 // import 'view/home_page.dart';
 // import 'view/clientes_page.dart';
@@ -16,7 +17,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        Provider<ClienteHomeController>(create: (_) => ClienteHomeController(),)
+      ],
+      child: MaterialApp(
       title: 'Agendamento',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
@@ -26,6 +31,7 @@ class MyApp extends StatelessWidget {
         // AppRoutes.PROFSSIONAIS: (context) => ProfissionaisPage(),
         '/': (context) => ClientesPage(),
       },
+    ),
     );
   }
 }
