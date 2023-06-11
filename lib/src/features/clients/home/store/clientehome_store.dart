9e035d1b639f4228d40d77cb303f8cb215ce8057
dev <adriana.bispo283@gmail.com>
2023-06-11 16:31:47 -0300

    refactore: renomedo o nome da classe

--- lib/src/features/clients/home/store/clientehome_store.dart
+++ lib/src/features/clients/home/store/clientehome_store.dart
@@ -1,13 +1,13 @@
-import 'package:agenda/repository/validator.dart';
+import '../../../../shared/utils/validator.dart';
 import 'package:mobx/mobx.dart';
 part 'clientehome_store.g.dart';
 
-class ClienteHomeController = _ClienteHomeStore with _$Controller;
+class ClienteHomeController = _ClienteHomeStore with _$ClienteHomeController;
 
 abstract class _ClienteHomeStore with Store {
   final validator = Validator();
   late List<ReactionDisposer> _disposers;
-  String? erroNome = '';
+  FormErrorState error = FormErrorState();
 
   @observable
   String nome = '';
@@ -29,6 +29,46 @@ abstract class _ClienteHomeStore with Store {
 
   @action
   void validateNome(String value) {
-    erroNome = Validator.isTextValid(value);
+    error.name = Validator.isTextValid(value);
   }
+
+  @action
+  void validateLasteName(String value){
+    error.lastname = Validator.isTextValid(value);
+  }
+
+  @action 
+  void validateTelephone(String value){
+    error.telephone = Validator.isTelefoneValid(value);
+  }
+
+  @action
+  void validateAll() {
+    validateNome(nome);
+    //validateLasteName(value);
+    validateTelephone(telefone);
+    /*
+    if(error.hasErrors) {
+
+    }
+     */
+  }
+
+}
+
+// ignore: library_private_types_in_public_api
+class FormErrorState = _FormErrorState with _$FormErrorState;
+
+abstract class _FormErrorState with Store {
+  @observable
+  String? name;
+
+  @observable
+  String? lastname;
+
+  @observable
+  String? telephone;
+
+  @computed
+  bool get hasErrors => name != null || lastname != null || telephone != null;
 }
--- lib/src/features/clients/home/store/clientehome_store.g.dart
+++ lib/src/features/clients/home/store/clientehome_store.g.dart
@@ -8,7 +8,7 @@ part of 'clientehome_store.dart';
 
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
-mixin _$Controller on _ClienteHomeStore, Store {
+mixin _$ClienteHomeController on _ClienteHomeStore, Store {
   late final _$nomeAtom =
       Atom(name: '_ClienteHomeStore.nome', context: context);
 
@@ -56,10 +56,110 @@ mixin _$Controller on _ClienteHomeStore, Store {
   }
 
   @override
+  void validateLasteName(String value) {
+    final _$actionInfo = _$_ClienteHomeStoreActionController.startAction(
+        name: '_ClienteHomeStore.validateLasteName');
+    try {
+      return super.validateLasteName(value);
+    } finally {
+      _$_ClienteHomeStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void validateTelephone(String value) {
+    final _$actionInfo = _$_ClienteHomeStoreActionController.startAction(
+        name: '_ClienteHomeStore.validateTelephone');
+    try {
+      return super.validateTelephone(value);
+    } finally {
+      _$_ClienteHomeStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void validateAll() {
+    final _$actionInfo = _$_ClienteHomeStoreActionController.startAction(
+        name: '_ClienteHomeStore.validateAll');
+    try {
+      return super.validateAll();
+    } finally {
+      _$_ClienteHomeStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
   String toString() {
     return '''
 nome: ${nome},
 telefone: ${telefone}
+    ''';
+  }
+}
+
+mixin _$FormErrorState on _FormErrorState, Store {
+  Computed<bool>? _$hasErrorsComputed;
+
+  @override
+  bool get hasErrors =>
+      (_$hasErrorsComputed ??= Computed<bool>(() => super.hasErrors,
+              name: '_FormErrorState.hasErrors'))
+          .value;
+
+  late final _$nameAtom = Atom(name: '_FormErrorState.name', context: context);
+
+  @override
+  String? get name {
+    _$nameAtom.reportRead();
+    return super.name;
+  }
+
+  @override
+  set name(String? value) {
+    _$nameAtom.reportWrite(value, super.name, () {
+      super.name = value;
+    });
+  }
+
+  late final _$lastnameAtom =
+      Atom(name: '_FormErrorState.lastname', context: context);
+
+  @override
+  String? get lastname {
+    _$lastnameAtom.reportRead();
+    return super.lastname;
+  }
+
+  @override
+  set lastname(String? value) {
+    _$lastnameAtom.reportWrite(value, super.lastname, () {
+      super.lastname = value;
+    });
+  }
+
+  late final _$telephoneAtom =
+      Atom(name: '_FormErrorState.telephone', context: context);
+
+  @override
+  String? get telephone {
+    _$telephoneAtom.reportRead();
+    return super.telephone;
+  }
+
+  @override
+  set telephone(String? value) {
+    _$telephoneAtom.reportWrite(value, super.telephone, () {
+      super.telephone = value;
+    });
+  }
+
+  @override
+  String toString() {
+    return '''
+name: ${name},
+lastname: ${lastname},
+telephone: ${telephone},
+hasErrors: ${hasErrors}
     ''';
   }
 }
