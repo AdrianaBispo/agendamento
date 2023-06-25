352935e4605a891f7cdf7cd478f550826ec09905
dev <adriana.bispo283@gmail.com>
2023-06-25 17:36:26 -0300

    refactore: tranformado em statefulWidget

--- lib/src/features/clients/newclient/components/inputname.dart
+++ lib/src/features/clients/newclient/components/inputname.dart
@@ -4,12 +4,32 @@ import 'package:flutter_modular/flutter_modular.dart';
 //controller
 import '../controller/newclient_controller.dart';
 
-class InputName extends StatelessWidget {
+class InputName extends StatefulWidget {
   InputName({Key? key}) : super(key: key);
 
+  @override
+  State<InputName> createState() => _InputNameState();
+}
+
+class _InputNameState extends State<InputName> {
   final _textController = TextEditingController();
 
   late final NewClientController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = Modular.get<NewClientController>();
+    controller.initialState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    controller.error.name = null;
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
