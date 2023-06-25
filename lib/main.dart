60216d86dc523a15d1762125899413d2c3cee53c
dev <adriana.bispo283@gmail.com>
2023-06-24 21:17:15 -0300

    feat: adcionado flutter modular

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:agenda/src/app_module.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'src/appwidget.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+//initializeDateFormatting().then((_) =>
 void main() {
-  initializeDateFormatting().then((_) => runApp(const MyApp()));
+  initializeDateFormatting().then(
+    (_) => runApp(
+      ModularApp(
+        module: AppModule(),
+        child: (const MyApp()),
+      ),
+    ),
+  );
 }
