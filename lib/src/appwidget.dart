8a0ba90a18e4a6e0705d73f5cc9de08432e288e0
dev <adriana.bispo283@gmail.com>
2023-06-25 16:56:38 -0300

    refactore: provider trocado pelo modular

--- lib/src/appwidget.dart
+++ lib/src/appwidget.dart
@@ -1,37 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-//view
-import 'features/clients/home/home.dart';
-//utils
-import 'features/clients/home/store/clientehome_store.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'shared/utils/app_theme.dart';
-// import 'view/home_page.dart';
-// import 'view/clientes_page.dart';
-// import 'view/profissionais_page.dart';
-// import 'view/agendamento_page.dart';
-//utils
-// import 'utils/routes.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<ClienteHomeController>(create: (_) => ClienteHomeController(),)
-      ],
-      child: MaterialApp(
+    return MaterialApp.router(
       title: 'Agendamento',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
-      routes: {
-        // AppRoutes.HOME: (context) => HomePage(),
-        // AppRoutes.CLIENTES: (context) => ClientesPage(),
-        // AppRoutes.PROFSSIONAIS: (context) => ProfissionaisPage(),
-        '/': (context) => ClientesPage(),
-      },
-    ),
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
     );
   }
 }
