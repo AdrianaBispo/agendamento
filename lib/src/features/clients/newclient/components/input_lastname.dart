b541d19d071afbe252325fde3f6f46d2ae70d0f0
dev <adriana.bispo283@gmail.com>
2023-06-25 17:33:41 -0300

    refactore: tranformado em statefulWidget

--- lib/src/features/clients/newclient/components/input_lastname.dart
+++ lib/src/features/clients/newclient/components/input_lastname.dart
@@ -4,12 +4,30 @@ import 'package:flutter_modular/flutter_modular.dart';
 //controller
 import '../controller/newclient_controller.dart';
 
-class InputLastName extends StatelessWidget {
-  InputLastName({Key? key}) : super(key: key);
+class InputLastName extends StatefulWidget {
+  const InputLastName({Key? key}) : super(key: key);
 
+  @override
+  State<InputLastName> createState() => _InputLastNameState();
+}
+
+class _InputLastNameState extends State<InputLastName> {
   final _textController = TextEditingController();
 
-  late final NewClientController controller;
+  late NewClientController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = Modular.get<NewClientController>();
+    controller.initialState();
+  }
+
+  @override
+  void dispose() {
+    controller.error.lastname = null;
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +40,7 @@ class InputLastName extends StatelessWidget {
         child: TextFormField(
           decoration: InputDecoration(
             labelText: 'Sobrenome',
-            errorText: controller.error.name,
+            errorText: controller.error.lastname,
           ),
           onChanged: (value) => controller.lastname = value,
           controller: _textController,
