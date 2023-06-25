11dd33ca3e7b9d517106f8434e69d2242b3bb296
dev <adriana.bispo283@gmail.com>
2023-06-25 16:21:02 -0300

    feat: criado clients module

---
+++ lib/src/features/clients/clients_module.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_modular/flutter_modular.dart';
+//contoller
+import 'home/store/clientehome_store.dart';
+import 'newclient/controller/newclient_controller.dart';
+//view
+import 'home/view/home.dart';
+import 'newclient/view/newclient.dart';
+
+class ClientsModule extends Module {
+  @override
+  List<Bind> get binds => [
+    Bind.singleton((i) => ClienteHomeController()),
+    Bind.singleton((i) => NewClientController())
+  ];
+
+  @override
+  List<ModularRoute> get routes => [
+    ChildRoute('/', child: (context, args) => ClientesPage()),
+    ChildRoute('/newclient', child: (context, args) => NewClient()),
+  ];
+}
