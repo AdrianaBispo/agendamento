096738f5791c7f796bb5e11561b8e94e896d92e8
dev <adriana.bispo283@gmail.com>
2023-09-12 21:56:22 -0300

    refacore: adcionada novas cores

--- lib/src/shared/utils/app_theme.dart
+++ lib/src/shared/utils/app_theme.dart
@@ -1,36 +1,45 @@
 import 'package:flutter/material.dart';
 //utils
 import 'app_color.dart';
+import 'app_textstyle.dart';
 
 class AppTheme {
   static ThemeData get theme {
     return ThemeData(
+      visualDensity: VisualDensity.adaptivePlatformDensity,
       fontFamily: 'Montserrat',
-      primaryColor: AppColor.instance.blue, 
-      dividerColor: AppColor.instance.natural_7,
-      scaffoldBackgroundColor: AppColor.instance.stronk,
+      primaryColor: AppColor.instance.primary,
+      dividerColor: AppColor.instance.accent3,
+      scaffoldBackgroundColor: AppColor.instance.primaryBackground,
+      textTheme: TextTheme(
+        headline6: TextStyle(
+          color: AppColor.instance.primaryText,
+          fontSize: 13,
+        ),
+      ),
       inputDecorationTheme: InputDecorationTheme(
         labelStyle: TextStyle(
-          color: AppColor.instance.black,
+          color: AppColor.instance.primaryText,
           fontSize: 14,
         ),
+        hintStyle: AppTextStyle.instance.hintText,
         contentPadding: const EdgeInsets.symmetric(
-          vertical: 10.0,
+          vertical: 20.0,
           horizontal: 8.0,
         ),
         isDense: true,
-        fillColor: AppColor.instance.white,
-        focusColor: AppColor.instance.white,
-        focusedBorder: UnderlineInputBorder(
+        fillColor: AppColor.instance.secondaryBackground,
+        focusColor: AppColor.instance.secondaryBackground,
+        focusedBorder: OutlineInputBorder(
           borderSide: BorderSide(
             width: 1,
-            color: AppColor.instance.natural_5,
+            color: AppColor.instance.accent2,
           ),
         ),
-        enabledBorder: UnderlineInputBorder(
+        enabledBorder: OutlineInputBorder(
           borderSide: BorderSide(
             width: 1.5,
-            color: AppColor.instance.natural_5,
+            color: AppColor.instance.accent2,
           ),
         ),
       ),
