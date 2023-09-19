a2c54141fa865748f030a0dc3aa6b98cab2d0a71
dev <adriana.bispo283@gmail.com>
2023-09-18 22:13:39 -0300

    refactore: import modificado

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:agenda/my_app.dart';
+/*import 'package:agenda/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -27,4 +27,4 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
-}
+}*/
