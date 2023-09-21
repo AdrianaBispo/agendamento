783780a7e9988f6fca26567dae632e30ea9c2e96
dev <adriana.bispo283@gmail.com>
2023-09-20 21:34:48 -0300

    refactore: adcionado future builder e data table cliente

--- lib/src/features/clients/home/view/home.dart
+++ lib/src/features/clients/home/view/home.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import '../../cliente_hive/client_adapter.dart';
-import '../../cliente_hive/cliente_hive.dart';
+import '../../../../shared/models/cliente.dart';
 //componentes
 import '../../../components/button_new.dart';
 import '../../../components/custom_appbar.dart';
+import '../components/data_table_client.dart';
+import '../../../components/circular_progress_custom.dart';
+
 class ClientesPage extends StatefulWidget {
   const ClientesPage({Key? key}) : super(key: key);
 
@@ -13,11 +16,8 @@ class ClientesPage extends StatefulWidget {
 }
 
 class _ClientesPageState extends State<ClientesPage> {
- 
-  //final _shopItem = Hive.openBox('cliente');
- //IClienteAdapter clienteadapter = Modular.get();
- ClienteHive clienteHive = Modular.get();
-   
+  Future<List<Cliente>> clienteHive =
+      Modular.get<IClienteAdapter>().readAllRegisters();
 
   @override
   Widget build(BuildContext context) {
@@ -28,17 +28,32 @@ class _ClientesPageState extends State<ClientesPage> {
             Expanded(
               flex: 5,
               child: SizedBox(
-                child: SingleChildScrollView( 
+                child: SingleChildScrollView(
                   child: Column(
                     children: <Widget>[
                       const CustomAppBar(texto: 'Clientes'),
-
-                      const ButtonNew(path: '/clients/newclient', text: 'Novo Cliente',),
-                      ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: 1,
-                        itemBuilder: (context, index) => Text(Modular.get<IClienteAdapter>().toString()),
+                      const ButtonNew(
+                        path: '/clients/newclient',
+                        text: 'Novo Cliente',
                       ),
+                      FutureBuilder<List<Cliente>>(
+                          future: clienteHive,
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const CenteredCircularProgress();
+                            } else if (!snapshot.hasData &&
+                                snapshot.data == null) {
+                              return const Text(
+                                'Adcione os seus primeiros clientes',
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w300),
+                              );
+                            } else {
+                              return DataTableCliente(
+                                  listClient: snapshot.data!);
+                            }
+                          }),
                     ],
                   ),
                 ),
