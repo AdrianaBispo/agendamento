930f0ec7491ea271765c0738ac0fc368c469f339
AdrianaBispo <adriana.bispo283@gmail.com>
2024-05-25 22:23:05 -0300

    :test_tube: Criado o test de GetallClientsUseCaseImp

---
+++ test/models/clients/domain/usecases/get_all_clients_usecase/get_all_clients_usecase_imp_test.dart
@@ -0,0 +1,30 @@
+import 'package:agenda/modules/clients/domain/entities/client_entity.dart';
+import 'package:agenda/modules/clients/domain/repositories/client_repository.dart';
+import 'package:agenda/modules/clients/domain/usecases/get_all_clients_usecase/get_all_clients_usecase_imp.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class ClientRepositoryMock extends Mock implements ClientRepository {}
+
+main() {
+  late ClientRepositoryMock clientRepository;
+  late GetallClientsUseCaseImp usecase;
+  late List<ClientEntity> expectedClients;
+
+  setUp(() {
+    clientRepository = ClientRepositoryMock();
+    usecase = GetallClientsUseCaseImp(clientRepository);
+
+    expectedClients = <ClientEntity>[];
+  });
+
+  test('deve retornar uma lista de ClientEntity', () async {
+    when(() => clientRepository.getAllClient())
+        .thenAnswer((_) async => Right(expectedClients));
+
+    final result = await usecase.call();
+
+    expect(result, equals(Right(expectedClients)));
+  });
+}
