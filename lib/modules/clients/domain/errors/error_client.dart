3012593c09ab0ecacad5f470bd1be35068717c91
AdrianaBispo <adriana.bispo283@gmail.com>
2024-05-25 22:08:31 -0300

    :sparkles: feat: adcionado error client

---
+++ lib/modules/clients/domain/errors/error_client.dart
@@ -0,0 +1,9 @@
+abstract class FailureGetAllClients implements Exception {}
+
+abstract class FailureGetClient implements Exception {}
+
+abstract class FailureCreateClient implements Exception {}
+
+abstract class FailureUpdateClient implements Exception {}
+
+abstract class FailureDeleteClient implements Exception {}
