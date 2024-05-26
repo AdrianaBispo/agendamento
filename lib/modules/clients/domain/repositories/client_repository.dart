8498f14b62161e150e495583564bd3fe7a99ec82
AdrianaBispo <adriana.bispo283@gmail.com>
2024-05-25 22:07:01 -0300

    :sparkles: feat: criado Client Repository

---
+++ lib/modules/clients/domain/repositories/client_repository.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:agenda/modules/clients/domain/entities/client_entity.dart';
+
+import '../errors/error_client.dart';
+
+abstract class ClientRepository {
+  Future<Either<FailureCreateClient, List<ClientEntity>>> getAllClient();
+  Future<Either<FailureGetClient,ClientEntity>> getClient(int id);
+  Future<Either<FailureCreateClient,ClientEntity>> createClient(ClientEntity clientEntity);
+  Future<Either<FailureUpdateClient,ClientEntity>> updateClient(ClientEntity clientEntity);
+  Future<Either<FailureDeleteClient,ClientEntity>> deleteClient(int id);
+}
