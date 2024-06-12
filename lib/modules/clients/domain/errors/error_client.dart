79c4340b552889acb017dfeb57f459bf1326269f
AdrianaBispo <adriana.bispo283@gmail.com>
2024-06-12 11:25:55 -0300

    feat: GetAllClientsException

--- lib/modules/clients/domain/errors/error_client.dart
+++ lib/modules/clients/domain/errors/error_client.dart
@@ -1,5 +1,10 @@
 abstract class FailureGetAllClients implements Exception {}
 
+class GetAllClientsException implements FailureGetAllClients {
+  final String message;
+  GetAllClientsException({required this.message});
+}
+
 abstract class FailureGetClient implements Exception {}
 
 abstract class FailureCreateClient implements Exception {}
