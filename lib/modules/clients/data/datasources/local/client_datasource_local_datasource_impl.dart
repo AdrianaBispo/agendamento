7f3a67cc9af37c86cc623a7f6c8fbc08f805e0df
AdrianaBispo <adriana.bispo283@gmail.com>
2024-06-11 22:29:55 -0300

    feat: criado ClientLocalDataSourceImpl

---
+++ lib/modules/clients/data/datasources/local/client_datasource_local_datasource_impl.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+import 'package:path/path.dart';
+
+import 'package:agenda/modules/clients/data/dtos/client_dto.dart';
+import '../client_datasource.dart';
+
+class ClientLocalDataSourceImpl implements ClientDataSource {
+  Database? _dataBase;
+  final _clienteStore = intMapStoreFactory.store('clientes');
+  late Directory _dir;
+
+  ClientLocalDataSourceImpl();
+
+  Future _initDb() async {
+    if (_dataBase != null) return _dataBase;
+    if (Platform.isAndroid || Platform.isIOS) {
+      _dir = await getApplicationDocumentsDirectory();
+    } else {
+      _dir = await Directory.systemTemp.createTemp();
+    }
+    final dBPath = join(_dir.path, 'Clientes.db');
+    _dataBase = await databaseFactoryIo.openDatabase(dBPath);
+    return _dataBase;
+  }
+
+  @override
+  Future<List<ClientDto>> getAll() async {
+    //read
+    // Finder object can also sort data.
+    final finder = Finder(sortOrders: [
+      //ordem alfabetica
+      SortOrder('name'),
+    ]);
+
+    final recordSnapshots = await _clienteStore.find(
+      await _initDb() as Database,
+      finder: finder,
+    );
+    // Making a List<ClientDto> out of List<RecordSnapshot>
+    return recordSnapshots.map((snapshot) {
+      return ClientDto.fromJson(snapshot.value);
+    }).toList();
+  }
+}
