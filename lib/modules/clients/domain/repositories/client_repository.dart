eb6e385f06b1b2b29ac0e2ea1c58e534f71976d3
AdrianaBispo <adriana.bispo283@gmail.com>
2024-06-12 11:10:00 -0300

    atualizado getAllClient

--- lib/modules/clients/domain/repositories/client_repository.dart
+++ lib/modules/clients/domain/repositories/client_repository.dart
@@ -4,7 +4,7 @@ import 'package:agenda/modules/clients/domain/entities/client_entity.dart';
 import '../errors/error_client.dart';
 
 abstract class ClientRepository {
-  Future<Either<FailureCreateClient, List<ClientEntity>>> getAllClient();
+  Future<Either<FailureGetAllClients, List<ClientEntity>>> getAllClient();
   Future<Either<FailureGetClient,ClientEntity>> getClient(int id);
   Future<Either<FailureCreateClient,ClientEntity>> createClient(ClientEntity clientEntity);
   Future<Either<FailureUpdateClient,ClientEntity>> updateClient(ClientEntity clientEntity);
