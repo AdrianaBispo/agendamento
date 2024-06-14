2e65a8b14b2713fa2c95679c7c7872b7c1a8e967
AdrianaBispo <86270127+AdrianaBispo@users.noreply.github.com>
2024-06-14 14:50:16 -0300

    :recycle: refactor: adcionado required em deleteClient

--- lib/modules/clients/data/datasources/client_datasource.dart
+++ lib/modules/clients/data/datasources/client_datasource.dart
@@ -2,5 +2,5 @@ import 'package:agenda/modules/clients/data/dtos/client_dto.dart';
 
 abstract class ClientDataSource{
   Future<List<ClientDto>> getAll();
-  Future<void> deleteClient(int id);
+  Future<void> deleteClient({required int id});
 }
