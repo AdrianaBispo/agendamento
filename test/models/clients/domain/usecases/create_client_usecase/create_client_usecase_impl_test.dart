3dd3ce5b9b1ec19a81c401853f1cda7e54e2a4f2
AdrianaBispo <adriana.bispo283@gmail.com>
2024-06-13 22:27:56 -0300

    :test_tube: test: criado o teste de create client usecase impl

---
+++ test/models/clients/domain/usecases/create_client_usecase/create_client_usecase_impl_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'package:agenda/modules/clients/domain/usecases/create_client_usecase/create_client_usecase_impl.dart';
+import 'package:agenda/modules/clients/domain/entities/client_entity.dart';
+import 'package:agenda/modules/clients/domain/repositories/client_repository.dart';
+
+class ClientRepositoryMock extends Mock implements ClientRepository {}
+
+main() {
+  late ClientRepositoryMock clientRepository;
+  late CreateClientUseCaseImpl usecase;
+  late ClientEntity expectedClient;
+
+  setUp(() {
+    clientRepository = ClientRepositoryMock();
+    usecase = CreateClientUseCaseImpl(clientRepository);
+
+    expectedClient = ClientEntity(1,
+        name: 'name', telephone: '+55 78 99999-9999', historic: []);
+  });
+
+  test('deve retornar uma lista de ClientEntity', () async {
+    when(() => clientRepository.createClient(expectedClient))
+        .thenAnswer((_) async => Right(expectedClient));
+
+    final result = await usecase.call(expectedClient);
+
+    expect(result, equals(Right(expectedClient)));
+  });
+}
