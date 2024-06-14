470db8f3a73853240faef94caeffaf717ef4cc6a
AdrianaBispo <adriana.bispo283@gmail.com>
2024-06-13 22:27:06 -0300

    feat: criado create cliet usecase impl

---
+++ lib/modules/clients/domain/usecases/create_client_usecase/create_client_usecase_impl.dart
@@ -0,0 +1,17 @@
+import 'package:agenda/modules/clients/domain/entities/client_entity.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../errors/error_client.dart';
+import '../../repositories/client_repository.dart';
+import 'create_client_usecase.dart';
+
+class CreateClientUseCaseImpl implements CreateClientUseCase {
+  final ClientRepository _clientRepository;
+
+  CreateClientUseCaseImpl(this._clientRepository);
+
+  @override
+  Future<Either<FailureCreateClient, void>> call(ClientEntity clientEntity) async{
+    return await _clientRepository.createClient(clientEntity);
+  }
+}
