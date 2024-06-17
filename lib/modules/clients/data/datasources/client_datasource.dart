1d3f0b83f86254ccdaab4d04e7a23222aa6f65ad
AdrianaBispo <86270127+AdrianaBispo@users.noreply.github.com>
2024-06-17 14:13:38 -0300

    :sparkles: feat: criado  getClient

--- lib/modules/clients/data/datasources/client_datasource.dart
+++ lib/modules/clients/data/datasources/client_datasource.dart
@@ -3,4 +3,5 @@ import 'package:agenda/modules/clients/data/dtos/client_dto.dart';
 abstract class ClientDataSource{
   Future<List<ClientDto>> getAll();
   Future<void> deleteClient({required int id});
+  Future<ClientEntity> getClient({required int id});
 }
