ffbf2c6a46f4970c64c8d8b9c0d3ef850bae6865
AdrianaBispo <86270127+AdrianaBispo@users.noreply.github.com>
2024-06-18 17:23:34 -0300

    :wastebasket: remove: deletado erro get all clients

--- lib/modules/clients/domain/errors/error_get_all_clients.dart
+++
@@ -1,6 +0,0 @@
-abstract class FailureGetAllClients implements Exception {}
-
-class GetAllClientsException implements FailureGetAllClients {
-  final String message;
-  GetAllClientsException({required this.message});
-}
