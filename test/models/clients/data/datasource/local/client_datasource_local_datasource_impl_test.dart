181aa54e610e735157be29815a6a0fe745ed2841
AdrianaBispo <86270127+AdrianaBispo@users.noreply.github.com>
2024-06-19 16:53:51 -0300

    test: adcionado test para getClient, updateClient e createClient

--- test/models/clients/data/datasource/local/client_datasource_local_datasource_impl_test.dart
+++ test/models/clients/data/datasource/local/client_datasource_local_datasource_impl_test.dart
@@ -11,16 +11,37 @@ void main() {
   });
 
   group('getAll', () {
-    test('Deve retornar uma lista de ClientDto ', () async {
+    test('Deve retornar uma lista de ClientDto ao chamar getAll', () async {
       final result = await dataSource.getAll();
       expect(result, isA<List<ClientDto>>());
-    }, timeout: const Timeout(Duration(minutes: 1)));
+    });
   });
 
   group('delete', () {
-    test('Deve retornar void quando deletar o registro') async{
+    test('Deve retornar void quando deletar o registro', () async{
       final result = await dataSource.call(1);
       expect(result, equals(Right(null)));
-    }
+    });
+  });
+
+  group('getClient', () {
+    test('Deve retornar um ClienteDto ao obter um cliente pelo id', () async{
+      final result = await dataSource.getClient(id: 1);
+      expect(result, isA<ClientDto>());
+    });    
+  });
+
+  group('updateClient', () {
+    test('Deve retornar um ClienteDto ao atualizado o cliente', () async{
+      final result = await dataSource.updateClient(client: client);
+      expect(result, isA<ClientDto>());
+    });    
+  });
+
+  group('createClient', () {
+    test('Deve retornar um ClienteDto ao criar um cliente', () async{
+      final result = await dataSource.createClient(client: client);
+      expect(result, isA<ClientDto>());
+    }); 
   });
 }
