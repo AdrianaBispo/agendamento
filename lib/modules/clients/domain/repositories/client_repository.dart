2992bb31e9f9e590e5d17a76ea690f88b1ba2dc9
AdrianaBispo <adriana.bispo283@gmail.com>
2024-06-19 22:31:01 -0300

    fix: adcionado required

--- lib/modules/clients/domain/repositories/client_repository.dart
+++ lib/modules/clients/domain/repositories/client_repository.dart
@@ -5,8 +5,8 @@ import '../errors/error_client.dart';
 
 abstract class ClientRepository {
   Future<Either<FailureGetAllClients, List<ClientEntity>>> getAllClient();
-  Future<Either<FailureGetClient,ClientEntity>> getClient(int id);
-  Future<Either<FailureCreateClient,ClientEntity>> createClient(ClientEntity clientEntity);
-  Future<Either<FailureUpdateClient,ClientEntity>> updateClient(ClientEntity clientEntity);
-  Future<Either<FailureDeleteClient,void>> deleteClient(int id);
+  Future<Either<FailureGetClient,ClientEntity>> getClient({required int id});
+  Future<Either<FailureCreateClient,ClientEntity>> createClient({required ClientEntity clientEntity});
+  Future<Either<FailureUpdateClient,ClientEntity>> updateClient({required ClientEntity clientEntity});
+  Future<Either<FailureDeleteClient,void>> deleteClient({required int id});
 }
