45410d61266193810b5939a918c2e74722d51955
adrianaSud <adriana.bispo283@gmail.com>
2024-09-24 16:59:23 -0300

    fix: importação

--- lib/modules/professional/domain/usecases/delete_professional_usecase/delete_professional_usecase.dart
+++ lib/modules/professional/domain/usecases/delete_professional_usecase/delete_professional_usecase.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 
-import '../../errors/error_prossional.dart';
+import '../../errors/error_professional.dart';
 
 abstract class DeleteProfessionalUseCase {
   Future<Either<FailureDeleteProfessional,void>> call(int id);
