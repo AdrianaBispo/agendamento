d081e0fa55a8e521845000a2c2293b0dcbb92503
AdrianaBispo <adriana.bispo283@gmail.com>
2024-09-24 20:44:20 -0300

    fix: corrigido import

--- lib/modules/professional/domain/usecases/create_professional_usecase/create_professional_usecase_impl.dart
+++ lib/modules/professional/domain/usecases/create_professional_usecase/create_professional_usecase_impl.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 
-import '../entities/professional_entity.dart';
 import '../../errors/error_professional.dart';
+import '../../entities/professional_entity.dart';
 import '../../repositories/professional_repository.dart';
 import 'create_professional_usecase.dart';
 
