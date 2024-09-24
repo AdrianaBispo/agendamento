605899b9f6ed6693fbd133042bd25f44f9790f9a
AdrianaBispo <adriana.bispo283@gmail.com>
2024-09-24 20:50:30 -0300

    fix: corrigido import

--- lib/modules/professional/domain/usecases/get_all_professional_usecase/get_all_professional_usecase_impl.dart
+++ lib/modules/professional/domain/usecases/get_all_professional_usecase/get_all_professional_usecase_impl.dart
@@ -1,10 +1,10 @@
 import 'package:dartz/dartz.dart';
 
-import '../entities/professional_entity.dart';
+import '../../entities/professional_entity.dart';
 import '../../errors/error_professional.dart';
 import '../../repositories/professional_repository.dart';
   
-import 'get_all_professionals_usecase.dart';
+import 'get_all_professional_usecase.dart';
 
 class GetallProfessionalUseCaseImpl implements GetallProfessionalUseCase {
   final ProfessionalRepository _professionalRepository;
