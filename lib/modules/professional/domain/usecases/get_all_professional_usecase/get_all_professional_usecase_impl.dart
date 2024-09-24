9632444c9663c115dc8ae50dd025912cdc95e515
adrianaSud <adriana.bispo283@gmail.com>
2024-09-24 17:05:20 -0300

    feat: criado get all professional usecase impl

---
+++ lib/modules/professional/domain/usecases/get_all_professional_usecase/get_all_professional_usecase_impl.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+
+import '../entities/professional_entity.dart';
+import '../../errors/error_professional.dart';
+import '../../repositories/professional_repository.dart';
+  
+import 'get_all_professionals_usecase.dart';
+
+class GetallProfessionalUseCaseImpl implements GetallProfessionalUseCase {
+  final ProfessionalRepository _professionalRepository;
+
+  GetallProfessionalUseCaseImpl(this._professionalRepository);
+
+  @override
+  Future<Either<FailureGetAllProfessional, List<ProfessionalEntity>>> call() async {
+    return await _professionalRepository.getAllProfessional();
+  }
+}
