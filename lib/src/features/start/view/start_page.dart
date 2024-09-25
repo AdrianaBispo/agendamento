1a5bba7d16d8d01a7287e982446e2b733a1b0ca9
AdrianaBispo <adriana.bispo283@gmail.com>
2024-09-25 13:42:49 -0300

    adcionado colocado responsividade no menu

--- lib/src/features/start/view/start_page.dart
+++ lib/src/features/start/view/start_page.dart
@@ -15,25 +15,52 @@ class StartPage extends StatefulWidget {
 
 class _StartPageState extends State<StartPage> {
   final controller = Modular.get<StartController>();
+  final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _drawerKey,
+      appBar: MediaQuery.of(context).size.width < 1200 //desktop
+          ? AppBar(
+              elevation: 0,
+              backgroundColor: AppColor.instance.primaryBackground,
+              leading: IconButton(
+                  onPressed: () {
+                    _drawerKey.currentState?.openDrawer();
+                  },
+                  icon: Icon(Icons.menu, color: AppColor.instance.primary)),
+              actions: const [],
+            )
+          : const PreferredSize(
+              preferredSize: Size.zero,
+              child: SizedBox(),
+            ),
+      drawer: Container(
+        height: MediaQuery.of(context).size.height,
+        color: AppColor.instance.secondaryBackground,
+        child: ListView(
+          children: <Widget>[
+            SideMenu(),
+          ],
+        ),
+      ),
       body: SafeArea(
         child: Row(
           children: [
-            Expanded(
-              flex: 1,
-              child: Container(
-                height: MediaQuery.of(context).size.height,
-                color: AppColor.instance.secondaryBackground,
-                child: ListView(
-                  children: <Widget>[
-                    SideMenu(),
-                  ],
+            if (MediaQuery.of(context).size.width >= 1200) //desktop
+              Expanded(
+                flex: 1,
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  color: AppColor.instance.secondaryBackground,
+                  child: ListView(
+                    children: <Widget>[
+                      SideMenu(),
+                    ],
+                  ),
                 ),
               ),
-            ),
             const Expanded(
               flex: 5,
               child: RouterOutlet(),
