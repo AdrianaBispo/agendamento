e98c78bea622a55ef9f2da1ebc1481fa2a1c924f
AdrianaBispo <adriana.bispo283@gmail.com>
2024-09-24 22:27:06 -0300

    feat: criado client controller

---
+++ lib/modules/clients/presenter/controller/client_controller.dart
@@ -0,0 +1,80 @@
+import 'dart:developer';
+import 'package:agenda/core/utils/validator.dart';
+import '../../../clients/data/dtos/client_dto.dart';
+import '../../data/datasources/local/client_datasource_local_datasource_impl.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
+
+part 'client_controller.g.dart';
+
+class ClientController = _ClientStore with _$ClientController;
+
+abstract class _ClientStore with Store {
+  final validator = Validator();
+  late List<ReactionDisposer> _disposers;
+  FormErrorState error = FormErrorState();
+  final ClientLocalDataSourceImpl clientLocalDataSource =  Modular.get<ClientLocalDataSourceImpl>();
+
+  @observable
+  String name = '';
+
+  @observable
+  String telephone = '';
+
+  void initialState() {
+    _disposers = [
+      reaction((_) => name, validateNome),
+      reaction((_) => telephone, validateTelephone),
+    ];
+  }
+
+  void dispose() {
+    for (final d in _disposers) {
+      d();
+    }
+  }
+
+  @action
+  void validateNome(String value) {
+    error.name = Validator.isTextValid(value);
+  }
+
+  @action
+  void validateTelephone(String value) {
+    error.telephone = Validator.isTelefoneValid(value);
+  }
+
+  @action
+  void validateAll() {
+    log('ValidateAll');
+    validateNome(name);
+    validateTelephone(telephone);
+
+    if (error.hasErrors == false) {
+      clientLocalDataSource.createClient(client: 
+        ClientDto(
+          null,
+          nameClient: name,
+          telephoneClient: telephone,
+          historicClient: [],
+        ),
+      );
+    }
+  }
+}
+
+// ignore: library_private_types_in_public_api
+class FormErrorState = _FormErrorState with _$FormErrorState;
+
+abstract class _FormErrorState with Store {
+  @observable
+  String? name;
+
+  @observable
+  String? telephone;
+
+  @computed
+  bool get hasErrors =>
+      name != null ||
+      telephone != null;
+}
