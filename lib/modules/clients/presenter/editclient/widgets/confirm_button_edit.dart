99d62e176a6370c89d175288f733783f049bc5d7
adrianaSud <adriana.bispo283@gmail.com>
2024-10-24 15:09:30 -0300

    feat: criado confim button

---
+++ lib/modules/clients/presenter/editclient/widgets/confirm_button_edit.dart
@@ -0,0 +1,42 @@
+import 'package:agenda/core/utils/app_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+
+class ConfirmButtonEdit extends StatelessWidget {
+  const ConfirmButtonEdit({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Modular.get<NewClientController>();
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 30, 0, 0),
+      child: TextButton(
+        label: Text(
+          'Editar cliente',
+          style: TextStyle(
+            color: AppColor.instance.secondaryBackground,
+          ),
+        ),
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.fromLTRB(70, 20, 70, 20),
+          minimumSize: const Size(350, 60),
+          backgroundColor: AppColor.instance.primary,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          ),
+        ),
+        onPressed: () {
+          controller.validateAll();
+          if (controller.error.hasErrors == false) {
+            controller.update();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Cliente editado com sucesso!')),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
