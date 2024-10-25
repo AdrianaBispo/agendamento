cbf2e5f510b82cadaab290e00642ea44a1ead713
AdrianaBispo <adriana.bispo283@gmail.com>
2024-10-24 21:52:17 -0300

    feat: alterado o caminho

---
+++ lib/modules/widgets/input_custom.dart
@@ -0,0 +1,34 @@
+import 'package:agenda/core/utils/app_textstyle.dart';
+import 'package:flutter/material.dart';
+
+class InputCustom extends StatelessWidget {
+  final String title;
+  final Widget child;
+  const InputCustom({
+    Key? key,
+    required this.child,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        bottom:20,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 3, bottom: 10),
+            child: Text(
+              title,
+              style: AppTextStyle.instance.titleLarge,
+            ),
+          ),
+          child,
+        ],
+      ),
+    );
+  }
+}
