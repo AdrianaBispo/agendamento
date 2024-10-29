76225c9df798154b1f41d84c923857ff0c5ed4a6
AdrianaBispo <adriana.bispo283@gmail.com>
2024-10-28 22:14:38 -0300

    refactore: adcionado client edit

--- lib/modules/clients/clients_module.dart
+++ lib/modules/clients/clients_module.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_modular/flutter_modular.dart';
 //contoller
+import 'presenter/editclient/controller/edit_client_controller.dart';
 import 'presenter/editclient/view/client_edit_page.dart';
+import 'presenter/home/controller/client_controller.dart';
 import 'presenter/newclient/controller/newclient_controller.dart';
 //view
 import 'presenter/home/view/client_view.dart';
@@ -9,14 +11,16 @@ import 'presenter/newclient/view/newclient_view.dart';
 class ClientsModule extends Module {
   @override
   List<Bind> get binds => [
+        Bind.singleton((i) => ClientController()),
         Bind.singleton((i) => NewClientController()),
+        Bind.singleton((i) => ClientEditController()),
       ];
 
   @override
   List<ModularRoute> get routes => [
         ChildRoute('/', child: (context, args) => const ClientesPage()),
         ChildRoute('/newclient', child: (context, args) => const NewClient()),
-        ChildRoute('/newclient',
+        ChildRoute('/editclient',
             child: (context, args) => ClienteEditPage(cliente: args.data)),
       ];
 }
