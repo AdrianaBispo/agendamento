2fb7dc8dea8db114e88facc3b89f207461471764
AdrianaBispo <adriana.bispo283@gmail.com>
2024-10-28 22:18:23 -0300

    removido o get id

--- lib/modules/clients/data/datasources/local/client_datasource_local_datasource_impl.dart
+++ lib/modules/clients/data/datasources/local/client_datasource_local_datasource_impl.dart
@@ -11,7 +11,6 @@ import '../client_datasource.dart';
 class ClientLocalDataSourceImpl implements ClientDataSource {
   Database? _dataBase;
   final _clientStore = intMapStoreFactory.store('client');
-  
 
   ClientLocalDataSourceImpl();
 
@@ -38,17 +37,18 @@ class ClientLocalDataSourceImpl implements ClientDataSource {
       await _initDatabase() as Database,
       finder: finder,
     );
-    final result =  recordSnapshots.map((snapshot) {
+    final result = recordSnapshots.map((snapshot) {
       return ClientDto.fromJson(snapshot.value);
     }).toList();
     return result;
   }
 
   @override
-  Future<ClientDto> getClient({required int id}) async{
+  Future<ClientDto> getClient({required int id}) async {
     final filter = Finder(filter: Filter.equals('id', id));
     var dataBase = await _initDatabase() as Database;
-    var snapshot = await _clientStore.query(finder: filter).getSnapshot(dataBase);
+    var snapshot =
+        await _clientStore.query(finder: filter).getSnapshot(dataBase);
     final result = ClientDto.fromJson(snapshot!.value);
     return result;
   }
@@ -56,14 +56,14 @@ class ClientLocalDataSourceImpl implements ClientDataSource {
   @override
   Future<void> deleteClient({required int id}) async {
     final find = Finder(filter: Filter.equals('id', id));
-    var dataBase =  await _initDatabase() as Database;
+    var dataBase = await _initDatabase() as Database;
     await _clientStore.delete(dataBase, finder: find);
   }
 
   @override
-  Future<ClientDto> updateClient({required ClientDto client}) async{
+  Future<ClientDto> updateClient({required ClientDto client}) async {
     final find = Finder(filter: Filter.equals('id', client.id));
-    var dataBase =  await _initDatabase() as Database;
+    var dataBase = await _initDatabase() as Database;
     await _clientStore.update(dataBase, client.toJson(), finder: find);
     var snapshot = await _clientStore.query(finder: find).getSnapshot(dataBase);
     final ClientDto result = ClientDto.fromJson(snapshot!.value);
@@ -71,21 +71,18 @@ class ClientLocalDataSourceImpl implements ClientDataSource {
   }
 
   @override
-  Future<ClientDto> createClient({required ClientDto client}) async{
-    var dataBase =  await _initDatabase() as Database;
-    var find = Finder(
+  Future<ClientDto> createClient({required ClientDto client}) async {
+    var dataBase = await _initDatabase() as Database;
+    /*var find = Finder(
       filter: Filter.equals("id", null),
-    );
+    );*/
 
     final result = await dataBase.transaction(
       (txn) async {
         // Add the object, get the auto incremented id
-        var key = await _clientStore.add(txn, client.toJson());
-        // Set the Id in our object
-        await _clientStore.update(txn, {'id': key}, finder: find);
+        await _clientStore.add(txn, client.toJson());
       },
     );
     return result;
   }
-
 }
