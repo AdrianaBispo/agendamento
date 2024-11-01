720e7fa8785d889c13cdf17895cbc024f2d6802d
AdrianaBispo <adriana.bispo283@gmail.com>
2024-10-31 21:34:39 -0300

    chore: alterado para ClientHomeView

--- lib/modules/clients/clients_module.dart
+++ lib/modules/clients/clients_module.dart
@@ -5,7 +5,7 @@ import 'presenter/editclient/view/client_edit_page.dart';
 import 'presenter/home/controller/client_controller.dart';
 import 'presenter/newclient/controller/newclient_controller.dart';
 //view
-import 'presenter/home/view/client_view.dart';
+import 'presenter/home/view/client_home_view.dart';
 import 'presenter/newclient/view/newclient_view.dart';
 
 class ClientsModule extends Module {
@@ -18,7 +18,7 @@ class ClientsModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute('/', child: (context, args) => const ClientesPage()),
+        ChildRoute('/', child: (context, args) => const ClientHomeView()),
         ChildRoute('/newclient', child: (context, args) => const NewClientView()),
         ChildRoute('/editclient',
             child: (context, args) => ClienteEditPage(cliente: args.data)),
