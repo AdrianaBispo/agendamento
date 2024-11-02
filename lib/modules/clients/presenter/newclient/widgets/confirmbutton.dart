76671f1ccd3101b9d5d0f6d1ecd7b02dcd26d88d
AdrianaBispo <adriana.bispo283@gmail.com>
2024-11-02 14:07:29 -0300

    refactor: removido padding

--- lib/modules/clients/presenter/newclient/widgets/confirmbutton.dart
+++ lib/modules/clients/presenter/newclient/widgets/confirmbutton.dart
@@ -5,48 +5,42 @@ import 'package:agenda/core/utils/app_color.dart';
 
 import '../controller/newclient_controller.dart';
 
-
-
 class ConfirmButton extends StatelessWidget {
   const ConfirmButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final controller = Modular.get<NewClientController>();
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(20, 30, 0, 0),
-      child: TextButton.icon(
-        icon: SvgPicture.asset(
-          'asset/icones/Icon/add.svg',
-          color: AppColor.instance.secondaryBackground,
-          width: 20,
-          height: 20,
-        ),
-        label: Text(
-          'Salvar',
-          style: TextStyle(
-            color: AppColor.instance.secondaryBackground,
-          ),
-        ),
-        style: TextButton.styleFrom(
-          padding: const EdgeInsets.fromLTRB(70, 20, 70, 20),
-          minimumSize: const Size(350, 60),
-          backgroundColor: AppColor.instance.primary,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(5.0)),
-          ),
-        ),
-        onPressed: () {
-          controller.validateAll();
-          if (controller.error.hasErrors == false) {
-            
-           Modular.to.navigate('./');
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Salvo com sucesso!')),
-            );
-          }
-        }, //onPressed
+    return TextButton.icon(
+      icon: SvgPicture.asset(
+        'asset/icones/Icon/add.svg',
+        color: AppColor.instance.secondaryBackground,
+        width: 20,
+        height: 20,
       ),
+      label: Text(
+        'Salvar',
+        style: TextStyle(
+          color: AppColor.instance.secondaryBackground,
+        ),
+      ),
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.fromLTRB(70, 20, 70, 20),
+        minimumSize: const Size(350, 60),
+        backgroundColor: AppColor.instance.primary,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        ),
+      ),
+      onPressed: () {
+        controller.validateAll();
+        if (controller.error.hasErrors == false) {
+          Modular.to.navigate('./');
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Salvo com sucesso!')),
+          );
+        }
+      },
     );
   }
 }
