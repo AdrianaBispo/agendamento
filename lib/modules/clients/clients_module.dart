6ec8f780c7edf501d1416945c0135cb026cb0a01
AdrianaBispo <adriana.bispo283@gmail.com>
2024-11-03 15:57:27 -0300

    chore: alterado a importação de client edit view

--- lib/modules/clients/clients_module.dart
+++ lib/modules/clients/clients_module.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_modular/flutter_modular.dart';
-//contoller
+//controller
 import 'presenter/editclient/controller/edit_client_controller.dart';
-import 'presenter/editclient/view/client_edit_page.dart';
 import 'presenter/home/controller/client_controller.dart';
 import 'presenter/newclient/controller/newclient_controller.dart';
 //view
 import 'presenter/home/view/client_home_view.dart';
 import 'presenter/newclient/view/newclient_view.dart';
+import 'presenter/editclient/view/client_edit_view.dart';
 
 class ClientsModule extends Module {
   @override
