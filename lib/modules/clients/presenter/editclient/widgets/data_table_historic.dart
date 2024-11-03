bfb8ae1073613d92ddb122b1a7baa55be65252bb
AdrianaBispo <adriana.bispo283@gmail.com>
2024-11-03 18:34:57 -0300

    chore: removido comentario

--- lib/modules/clients/presenter/editclient/widgets/data_table_historic.dart
+++ lib/modules/clients/presenter/editclient/widgets/data_table_historic.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../../../../core/theme/app_color.dart';
 import '../../../../../core/theme/app_textstyle.dart';
 import '../../../../historic/dtos/historic_dto.dart';
 
@@ -34,9 +33,6 @@ class DataTableHistoric extends StatelessWidget {
             style: AppTextStyle.instance.dataColumn,
           ),
         ),
-        const DataColumn(
-          label: Text(''),
-        ), //editar
       ],
       rows: List.generate(
         listHistoric.length,
@@ -69,84 +65,6 @@ class DataTableHistoric extends StatelessWidget {
             child: Text(
               description,
               style: AppTextStyle.instance.dataCell,
-            ),
-          ),
-        ),
-        DataCell(
-          IconButton(
-            onPressed: () {},
-            /*=> showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: const Padding(
-                      padding: EdgeInsets.only(left: 20.0),
-                      child: Text(
-                        'Deletar historico',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 17,
-                            color: AppColor.natural),
-                      ),
-                    ),
-                    content: Container(
-                      width: MediaQuery.of(context).size.width / 2,
-                      padding: const EdgeInsets.fromLTRB(20, 0, 10, 20),
-                      child: const Text(
-                        'Você tem certeza que deseja deletar o historico desse serviço? ',
-                        style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 15,
-                            color: AppColor.natural),
-                      ),
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        child: const Text(
-                          'Não',
-                          style: TextStyle(
-                            color: AppColor.white,
-                          ),
-                        ),
-                        style: TextButton.styleFrom(
-                          padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
-                          backgroundColor: AppColor.red,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(5.0)),
-                          ),
-                        ),
-                        onPressed: () => Navigator.of(context).pop(),
-                      ),
-                      TextButton(
-                        child: const Text(
-                          'Sim',
-                          style: TextStyle(
-                            color: AppColor.white,
-                          ),
-                        ),
-                        style: TextButton.styleFrom(
-                          padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
-                          backgroundColor: AppColor.grenn,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(5.0)),
-                          ),
-                        ),
-                        onPressed: () {
-                          setState(() => _listaHistorico.removeAt(index));
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  );
-                  
-                }),*/
-            //editar o name e a profissão
-            icon: Icon(
-              Icons.delete_sharp,
-              color: AppColor.instance.secondaryText,
-              size: 20.0,
             ),
           ),
         ),
