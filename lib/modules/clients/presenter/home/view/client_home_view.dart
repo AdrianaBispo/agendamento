b6a61fe1cf08841d0a6306001a11d4b9eaa40430
AdrianaBispo <adriana.bispo283@gmail.com>
2025-03-19 21:32:39 -0300

    refactorr: adcionado Observer

--- lib/modules/clients/presenter/home/view/client_home_view.dart
+++ lib/modules/clients/presenter/home/view/client_home_view.dart
@@ -10,15 +10,10 @@ import '../widgets/button_newclient.dart';
 import '../../../../widgets/custom_app_bar.dart';
 import '../../../../widgets/data_empity.dart';
 
-class ClientHomeView extends StatefulWidget {
-  const ClientHomeView({Key? key}) : super(key: key);
+class ClientHomeView extends StatelessWidget {
+  ClientHomeView({Key? key}) : super(key: key);
 
-  @override
-  State<ClientHomeView> createState() => _ClientHomeViewState();
-}
-
-class _ClientHomeViewState extends State<ClientHomeView> {
-  ClientController clientController = Modular.get<ClientController>();
+  final ClientController clientController = Modular.get<ClientController>();
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +30,8 @@ class _ClientHomeViewState extends State<ClientHomeView> {
                       const CustomAppBar(texto: 'Clientes'),
                       const ButtonNewClient(),
                       Observer(
-                        builder: (context) {
-                          if (!clientController.hasResults) {
+                        builder: (_) {
+                          if (clientController.loading) {
                             return const SizedBox(
                               width: 500.0,
                               height: 300.0,
@@ -52,14 +47,14 @@ class _ClientHomeViewState extends State<ClientHomeView> {
                               ),
                             );
                           } else if (clientController.repositories.isEmpty) {
-                          return const EmpityData(
-                            text: 'Adcione os seus primeiros clientes',
-                          );
-                        } else {
-                          return DataTableCliente(
-                            listClient: clientController.repositories,
-                          );
-                        }
+                            return const EmpityData(
+                              text: 'Adcione os seus primeiros clientes',
+                            );
+                          } else {
+                            return DataTableCliente(
+                              listClient: clientController.repositories,
+                            );
+                          }
                         },
                       ),
                     ],
