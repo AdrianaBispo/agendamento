ac8adb2ab1639924fa96eeccbd5572d897cba043
AdrianaBispo <adriana.bispo283@gmail.com>
2025-03-22 10:19:18 -0300

    refactor: adcionado NewProfessional Controller e EditProfessionalController

--- lib/modules/professional/professional_module.dart
+++ lib/modules/professional/professional_module.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+//data
 import 'data/datasources/local/professional_datasource_local_datasource_impl.dart';
 import 'data/repositories/professional_repository_impl.dart';
+//presenter
 import 'presenter/home/controller/professional_controller.dart';
 import 'presenter/home/view/professional_view.dart';
-//data
-//presenter
+import 'presenter/edit_professional/controller/edit_professional_controller.dart';
+import 'presenter/new_professional/controller/new_professional_controller.dart';
 
 class ProfessionalModule extends Module {
   @override
@@ -13,6 +15,10 @@ class ProfessionalModule extends Module {
         Bind((i) => ProfessionalLocalDataSourceImpl()),
         Bind((i) => ProfessionalRepositoryImpl(i.get<ProfessionalLocalDataSourceImpl>())),
         Bind((i) => ProfessionalController(i.get<ProfessionalRepositoryImpl>())),
+        //NewProfessional
+        Bind((i) => NewProfessionalController(i.get<ProfessionalRepositoryImpl>())),
+        //EditProfessional
+        Bind((i) => EditProfessionalController(i.get<ProfessionalRepositoryImpl>())),
       ];
 
   @override
