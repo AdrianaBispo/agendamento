d9d03e900fd16e795ccd27c871d0fe06d2a0b9ee
AdrianaBispo <adriana.bispo283@gmail.com>
2025-04-07 20:34:51 -0300

    refactor: adcionado o setInitialRoute

--- lib/appwidget.dart
+++ lib/appwidget.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Modular.setInitialRoute('/clients/');
     return MaterialApp.router(
       title: 'Agendamento',
       debugShowCheckedModeBanner: false,
