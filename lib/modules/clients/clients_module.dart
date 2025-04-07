e8fafbdb06c98d295c0887dedba99d90f7abf125
AdrianaBispo <adriana.bispo283@gmail.com>
2025-04-07 20:34:24 -0300

    chore: Modular atualizado para o 6

--- lib/modules/clients/clients_module.dart
+++ lib/modules/clients/clients_module.dart
@@ -3,6 +3,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'data/repositories/client_repository_impl.dart';
 import 'data/datasources/local/client_datasource_local_datasource_impl.dart';
 //presenter
+import 'domain/repositories/client_repository.dart';
 import 'presenter/editclient/controller/edit_client_controller.dart';
 import 'presenter/home/controller/client_controller.dart';
 import 'presenter/home/view/client_home_view.dart';
@@ -12,22 +13,21 @@ import 'presenter/newclient/view/newclient_view.dart';
 
 class ClientsModule extends Module {
   @override
-  List<Bind> get binds => [
-        Bind((i) => ClientLocalDataSourceImpl()),
-        Bind((i) => ClientRepositoryImpl(i.get<ClientLocalDataSourceImpl>())),
-        Bind((i) => ClientController(i.get<ClientRepositoryImpl>())),
-        //NewClient
-        Bind((i) => NewClientController(i.get<ClientRepositoryImpl>())),
-        //EditClient
-        Bind((i) => ClientEditController(i.get<ClientRepositoryImpl>())),
-      ];
+  void binds(i) {
+    i.add(ClientLocalDataSourceImpl.new);
+    i.addSingleton<ClientRepository>(
+        () => ClientRepositoryImpl(i.get<ClientLocalDataSourceImpl>()));
+        
+    i.add(() => ClientController(i.get<ClientRepositoryImpl>()));
+    i.add(() => NewClientController(i.get<ClientRepositoryImpl>()));
+    i.add(() => ClientEditController(i.get<ClientRepositoryImpl>()));
+  }
 
   @override
-  List<ModularRoute> get routes => [
-        ChildRoute('/', child: (context, args) => ClientHomeView()),
-        ChildRoute('/newclient',
-            child: (context, args) => const NewClientView()),
-        ChildRoute('/editclient',
-            child: (context, args) => ClienteEditView(cliente: args.data)),
-      ];
+  void routes(r) {
+    r.child('/', child: (context) => ClientHomeView());
+    r.child('/newclient', child: (context) => NewClientView());
+    r.child('/editclient',
+        child: (context) => ClienteEditView(cliente: r.args.data));
+  }
 }
